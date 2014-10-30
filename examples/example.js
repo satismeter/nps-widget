e98210c0d82b83ba07b89835df5ace308250044e
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2014-10-30 21:18:18 +0100

    example in top-right corner

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
 var View = require('../src/index.js');
-var view = new View({data: {language: 'en', position: 'br'}});
+var view = new View({data: {language: 'en', position: 'tr'}});
 view.$appendTo(document.body);
 view.show();
